b5d124a9f71026be63ad13182ffad1f63d438ae7
vgonsie <vgonsie626@fp.iesromerovargas.com>
2025-04-22 14:08:22 +0200

    Delete lab directory

--- lab/BorraAtaque1.sh
+++
@@ -1,3 +0,0 @@
-virsh destroy ataque1
-virsh undefine ataque1
-rm -f /var/lib/libvirt/images/ataque1.qcow2
--- lab/CreaAtaque1.sh
+++
@@ -1,7 +0,0 @@
-virt-install --name ataque1 \
---ram 2048 --vcpus 2 \
---disk path=/var/lib/libvirt/images/ataque1.qcow2,size=10 \
---os-type linux --os-variant ubuntu22.04 \
---network bridge=virbr0 \
---cdrom /var/lib/libvirt/boot/ubuntu.iso \
---graphics none --console pty,target_type=serial
